147c7f3be1aa7413bc4ca829754a14f622a94f06
cuburt <cuburt4798@gmail.com>
2020-09-03 08:14:21 +0800

    collection and amortization pt2

--- credit_account/models/models.py
+++ credit_account/models/models.py
@@ -2,19 +2,8 @@
 
 from odoo import models, fields, api
 
-# class credit_account(models.Model):
-#     _name = 'credit_account.credit_account'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
-
 class PaymentTerm(models.Model):
     _inherit = 'account.payment.term'
 
     #TODO: Add payment terms here
+
